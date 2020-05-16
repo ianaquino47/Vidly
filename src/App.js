0799086c4f74699ff119ca157cc56b04c77d191a
iAquino47 <aquinoian@icloud.com>
2020-05-16 23:43:54 +0100

    displays movies in a table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Movies from './components/Movies'
 import './App.css';
+
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello world</h1>
+      <Movies/>
     </main>
   );
 }
