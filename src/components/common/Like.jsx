7b00b3d5940e05d9c1e74d56713c1494e0b3793d
iAquino47 <aquinoian@icloud.com>
2020-05-19 00:39:06 +0100

    added liked feature

---
+++ src/components/common/Like.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Like = (props) => {
+    const {liked, onClick } = props;
+    let classes = liked ? "fa fa-heart" : "fa fa-heart-o";
+    return (
+      <i
+        onClick={onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      ></i>
+    );
+}
+
+export default Like;
