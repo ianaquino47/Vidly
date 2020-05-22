9c7ddd7683ab469a902023f7ebd697aa6880c0cb
iAquino47 <aquinoian@icloud.com>
2020-05-23 00:05:42 +0100

    sorted keys

--- src/components/common/TableBody.jsx
+++ src/components/common/TableBody.jsx
@@ -4,7 +4,11 @@ import _ from "lodash";
 class TableBody extends Component {
   renderCell = (item, column) => {
     if (column.content) return column.content(item);
-    return _.get(item, column.path)
+    return _.get(item, column.path);
+  };
+
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key);
   };
 
   render() {
@@ -13,15 +17,14 @@ class TableBody extends Component {
     return (
       <tbody>
         {data.map((item) => (
-          <tr>
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
-        <tr>
-          <td></td>
-        </tr>
       </tbody>
     );
   }
