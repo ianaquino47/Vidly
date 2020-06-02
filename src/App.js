da5c3fb172967766b9ab6c3d2ef9e291eac6ed5e
iAquino47 <aquinoian@icloud.com>
2020-06-02 23:48:10 +0100

    added some routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Movies from './components/Movies'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import Movies from './components/Movies';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import NotFound from './components/NotFound';
 import './App.css';
 
 
 function App() {
   return (
     <main className="container">
-      <Movies/>
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route> 
+        <Redirect from="/" exact to="/movies"/>
+        <Redirect to="/not-found"/>
+      </Switch>
     </main>
   );
 }
