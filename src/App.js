4a55f39c1016556fc35ddf77afffe3d40d580f32
iAquino47 <aquinoian@icloud.com>
2020-06-03 00:08:01 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Movies from './components/Movies';
 import Customers from './components/Customers';
 import Rentals from './components/Rentals';
 import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 import './App.css';
 
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route> 
-        <Redirect from="/" exact to="/movies"/>
-        <Redirect to="/not-found"/>
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar/>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route> 
+          <Redirect from="/" exact to="/movies"/>
+          <Redirect to="/not-found"/>
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
