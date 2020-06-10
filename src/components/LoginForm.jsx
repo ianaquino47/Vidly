353d418661f0c269fea136cb3f3e452eabc23a16
iAquino47 <aquinoian@icloud.com>
2020-06-10 22:32:46 +0100

    basic validation implementation

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,10 +7,27 @@ class LoginForm extends Component {
       username: "",
       password: "",
     },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+    
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors)
+    this.setState({ errors });
+    if (errors) return;
 
     //Call the server
     console.log("submitted");
