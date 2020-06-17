771781174d8d42b00f005c13756c6fd7786b7774
iAquino47 <aquinoian@icloud.com>
2020-06-17 15:52:31 +0100

    extracted helper rendering methods

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from './common/Form'
-import Input from "./common/Input";
 
 class LoginForm extends Form {
   state = {
@@ -17,36 +16,19 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  
-
   doSubmit = () => {
     //Call the server
     console.log("submitted");
   }
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password', "password")}
+          {this.renderButton('Login')}
         </form>
       </div>
     );
